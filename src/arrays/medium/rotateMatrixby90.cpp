3a8d5a5bece11e1f0374a6f2513e93f2353f2699
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-01-12 11:56:30 +0530

    added faq

--- src/arrays/medium/rotateMatrixby90.cpp
+++ src/arrays/medium/rotateMatrixby90.cpp
@@ -1,6 +1,6 @@
 // statement- rotate n*n 2D matrix by 90 deg clockwise. Rotation must be done in place i.e input matrix must be modified directly
 
-// method1(brute)  t.c- O(n^2)  s.c- O(n^2)
+//method1(brute)  t.c- O(n^2)  s.c- O(n^2)
 void rotate(vector<vector<int>> &matrix){
     int n = matrix.size();
     vector<vector<int>> ans(n, vector<int>(n));
@@ -12,7 +12,7 @@ void rotate(vector<vector<int>> &matrix){
     matrix = ans;
 }
 
-// method2(optimal)  t.c- O( (n\2 * n\2) + (n * n\2) )  s.c- O(1)
+//method2(optimal)  t.c- O( (n\2 * n\2) + (n * n\2) )  s.c- O(1)
 void rotateClockwise(vector<vector<int>> &matrix){
     int n = matrix.size();
     //Transpose the matrix - (diagonal elements remain same while we transpose)
@@ -26,5 +26,25 @@ void rotateClockwise(vector<vector<int>> &matrix){
     for (int i = 0; i < n; ++i){
         // Reverse the current row to complete clockwise rotation
         reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+//FAQ- rotate n*n 2D matrix by 90 deg anti-clockwise
+//for anti-cloc we reverse it first n then transpose
+
+//(optimal)  t.c- O( (n\2 * n\2) + (n * n\2) )  s.c- O(1)
+void rotateClockwise(vector<vector<int>> &matrix){
+    int n = matrix.size();
+    // Reverse each row to get the req matrix
+    for (int i = 0; i < n; ++i){
+        // Reverse the current row to complete clockwise rotation
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+    //Transpose the matrix - (diagonal elements remain same while we transpose)
+    for (int i = 0; i < n; ++i){  //n-1 is done as we need to swap elements above diagonal only
+        for (int j = i+1; j < n; ++j){
+            // Swap element at (i, j) with (j, i) to transpose
+            swap(matrix[i][j], matrix[n-1-i][j]);
+        }
     }
 }
