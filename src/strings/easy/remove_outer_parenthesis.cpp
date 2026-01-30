6de545e7fe8495588639fb04c3d7a795556e6101
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-01-30 18:51:13 +0530

    added 1 que of strings

---
+++ src/strings/easy/remove_outer_parenthesis.cpp
@@ -0,0 +1,42 @@
+// statement- remove outermost parentheses from every primitive component of s & return resulting string
+// note: primitive valid parentheses string is non-empty valid string that cant be split into 2 or more non-empty valid parentheses strings
+
+// method1(brute) t.c- O(n)  s.c- O(n)
+string removeOuterParentheses(string s){
+    stack<char> st; 
+    string ans = "";
+
+    for (int i=0;i<s.size();i++){
+        if (s[i] == '('){
+            if (!st.empty()){  //when stack is not empty 
+                ans += s[i]; //add '(' to ans
+            }
+            st.push(s[i]); //when stack empty
+        }
+        else if (s[i] == ')'){
+            st.pop(); //remove prev added '(' as we get a pair of ()
+            if (!st.empty()){  //when stack is not empty 
+                ans += s[i]; //add ')' to ans
+            }
+        }
+    }
+    return ans;
+}
+
+// method2(optimal) t.c- O(n)  s.c- O(1)
+string removeOuterParentheses(string s){
+    string ans = "";
+    int cnt = 0;
+    // add parenthesis to ans only when cnter is > 0
+    for (int i = 0; i < s.size(); i++){
+        if (s[i] == '('){
+            if (cnt > 0) ans += s[i]; // check 1st & incr cnt later as we dont wanna add the 1st outer (
+            cnt++;  // incr the cnter for '('
+        }
+        else{ // for ')' parenthesis
+            cnt--; // decr the cnter for ')'
+            if (cnt > 0) ans += s[i];
+        }
+    }
+    return ans;
+}
