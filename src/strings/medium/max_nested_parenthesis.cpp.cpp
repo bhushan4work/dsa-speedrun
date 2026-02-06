ddec908f0f683bea1102f8c0e7b98c059c5da430
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-02-06 17:49:24 +0530

    added 1 string que

---
+++ src/strings/medium/max_nested_parenthesis.cpp.cpp
@@ -0,0 +1,17 @@
+// statement- given valid parentheses string s, return nesting depth of s. The nesting depth is max no of nested parentheses
+
+// method1(optimal) t.c- O(n)  s.c- O(1)
+int maxDepth(string s){
+    int ans = 0; //store max nested depth cnt of string
+    int currCnt = 0; //max depth seen so far
+    for (int i = 0; i < s.size(); i++){
+        if (s[i] == '('){
+            currCnt++; //decr currCnter on )
+        }
+        else if (s[i] == ')'){ 
+            currCnt--; //decr currCnter on )
+        }
+        ans = max(ans, currCnt); //update max depth if curr depth is greater
+    }
+    return ans;
+}
