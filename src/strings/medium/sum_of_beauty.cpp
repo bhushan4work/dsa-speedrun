486def49a0ec676ab17d5ef485ca5750a74e4dac
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-02-23 18:28:26 +0530

    added med string que

---
+++ src/strings/medium/sum_of_beauty.cpp
@@ -0,0 +1,26 @@
+// statement- beauty of str: difference betn most frequent char & least freq char. return sum of beauty values of all possible substr
+
+// method1(optimal) t.c- O(n^2)  s.c- O(26)
+int beautySum(string s){
+    int ans = 0;
+
+    for (int i = 0; i < s.size(); i++){
+        vector<int> freq(26, 0);
+
+        for (int j = i; j < s.size(); j++){
+            freq[s[j] - 'a']++;
+
+            int maxi = 0;
+            int mini = INT_MAX;
+
+            for (int c = 0; c < 26; c++){
+                if (freq[c] > 0){ // Find max and min frequency (ignore zeros)
+                    maxi = max(maxi, freq[c]);
+                    mini = min(mini, freq[c]);
+                }
+            }
+            ans += (maxi - mini); //calculate sum for curr substr
+        }
+    }
+    return ans;
+}
