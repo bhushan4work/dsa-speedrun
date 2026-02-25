9bb78f8f6fda4ccc0e2cd0dbc87d24252e098993
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-02-25 18:35:08 +0530

    added 2 LL que -easy

---
+++ src/LL/1D-LL/lengthOfLL.cpp
@@ -0,0 +1,14 @@
+// statement-  given the head of a LL, print length of the LL
+
+// method1(optimal) t.c- O(n)  s.c- O(1)
+int lengthOfLinkedList(Node *head){
+    int count = 0;
+
+    Node *temp = head; // Initialize a temp pointer to head
+
+    while (temp != nullptr){
+        count++; // Increment count for each node
+        temp = temp->next; // Move to the next node
+    }
+    return count;
+}
---
+++ src/LL/1D-LL/search_in_LL.cpp
@@ -0,0 +1,14 @@
+// statement- given the head of LL & an integer k, find out whether the k is present in LL or not. Return true if present else false
+
+// method1(optimal) t.c- O(n)  s.c- O(1)
+int searchInLinkedList(Node<int>* head, int k) {
+    Node<int>* current = head; // Pointer to traverse the list
+
+    while (current != nullptr){
+        if (current->data == k){ // Check if current node matches key
+            return 1; // Return true if found
+        }
+        current = current->next; // Move to next node
+    }
+    return 0; // Return false if not found
+}
