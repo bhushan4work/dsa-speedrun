3fa5f297f77d69ec13468e630bd6f64634b1608b
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-03-24 09:10:39 +0530

    DLL-mid

---
+++ src/LL/DLL-mid/delete_all_occu_of_key.cpp
@@ -0,0 +1,32 @@
+//statement- given head of DLL & Key. task is to delete all occurrences of given key if its present & return new DLL
+
+
+//method1(optimal) t.c- O(n)  s.c- O(1)
+Node* deleteAllOccurrences(Node* head, int key) {
+    Node* curr = head;
+
+    while (curr != nullptr) {
+        if (curr->data == key) {
+
+            Node* temp = curr;              // store node to be deleted
+            Node* prevNode = curr->back;
+            Node* nextNode = curr->next;
+
+            // If key is head,move head to next node & remove linking 
+            if (curr == head) {
+                head = curr->next;
+                if (head) head->back = nullptr;
+            } 
+
+            if (prevNode) prevNode->next = nextNode;
+            if (nextNode) nextNode->back = prevNode;
+
+            curr = curr->next;   // move BEFORE deleting the node
+            delete temp;
+        }
+        else{
+            curr = curr->next;
+        }
+    }
+    return head;
+}
