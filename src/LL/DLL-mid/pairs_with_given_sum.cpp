13297946d34f25e3bc49684c9a68f51b2ef4d757
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-03-25 09:29:40 +0530

    dll-mid

---
+++ src/LL/DLL-mid/pairs_with_given_sum.cpp
@@ -0,0 +1,53 @@
+// statement- given sorted DLL of +ve distinct elements, task is to find pairs in DLL whose sum = given value target
+
+// method1(brute) t.c- O(n^2)  s.c- O(1)
+vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
+    vector<pair<int, int>> ans;
+    Node *temp1 = head;
+
+    while (temp1 != nullptr){
+        Node *temp2 = temp1->next;
+
+        // we check for target only if sum<=target as list is sorted so theres no need to check further
+        while (temp2 != nullptr && (temp1->data + temp2->data <= target) ){
+            if (temp1->data + temp2->data == target){
+                ans.push_back({temp1->data, temp2->data});
+            }
+            temp2 = temp2->next;
+        }
+        temp1 = temp1->next;
+    }
+    return ans;
+}
+
+
+// method2(optimal) t.c- O(n + n)  s.c- O(1)
+Node* findTail(Node* head){
+    Node* tail = head;
+    while(tail->next != nullptr){
+        tail = tail->next;
+    }
+    return tail;
+}
+
+vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target){
+    vector<pair<int, int>> ans ;
+    if(head == nullptr) return ans; 
+    Node* left = head;
+    Node* right = findTail(head);
+
+    while(left->data < right->data){
+        if(left->data + right->data == target){ //once target found move both ptr
+            ans.push_back({left->data, right->data});
+            left = left->next;
+            right = right->prev;
+        }
+        else if (left->data + right->data < target){ //sum is less so move left ptr
+            left = left->next;
+        }
+        else{ //sum is greater so move right ptr
+            right = right->prev;
+        }
+    }
+    return ans;
+}
