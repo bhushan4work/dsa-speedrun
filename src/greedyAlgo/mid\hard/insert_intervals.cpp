17e8e6de84ee01bff5ef011f4a24c31c07a5a585
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-04-01 10:16:54 +0530

    greedy mid

---
+++ src/greedyAlgo/mid\hard/insert_intervals.cpp
@@ -0,0 +1,32 @@
+// statement- Insert newInterval arr into intervals arr such that intervals is still sorted in ascending order by starti & intervals still doesnt have any overlapping intervals (merge overlapping intervals if necessary)
+
+
+// method1(optimal) t.c- O(n)  s.c- O(n)
+vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int> &newInterval){
+    int n = intervals.size(); // Total no of existing intervals
+    vector<vector<int>> res;  // Result vector to store final merged intervals
+
+    int i = 0;
+
+    // Add all intervals that come completely before newInterval (i.e., their end < start of newInterval)
+    while (i < n && intervals[i][1] < newInterval[0]){
+        res.push_back(intervals[i]);
+        i++;
+    }
+
+    // Merge all overlapping intervals with newInterval. Overlap cond: interval start <= newInterval end
+    while (i < n && intervals[i][0] <= newInterval[1]){
+        // Update newInterval to cover the merged range
+        newInterval[0] = min(newInterval[0], intervals[i][0]);
+        newInterval[1] = max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    res.push_back(newInterval); // Add the merged interval
+
+    //  Add all remaining intervals (those after newInterval)
+    while (i < n){
+        res.push_back(intervals[i]);
+        i++;
+    }
+    return res; // Return final list of intervals
+}
---
+++ src/greedyAlgo/mid\hard/merge_interval.cpp
@@ -0,0 +1,42 @@
+// statement-
+
+// method1(brute) t.c- O(nlogn + n^2)  s.c- O(n)
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    int n = intervals.size();
+    sort(intervals.begin(), intervals.end()); //  Sort intervals based on starting time
+    vector<vector<int>> res;
+
+    for (int i = 0; i < n; i++) {
+        int start = intervals[i][0];
+        int end = intervals[i][1];
+
+        // Try merging with upcoming intervals
+        for (int j = i + 1; j < n; j++) {
+            if (intervals[j][0] <= end) { // If overlapping exist, extend curr interval
+                end = max(end, intervals[j][1]);
+            }
+            else{
+                break; // no more overlap possible (since sorted)
+            }
+        }
+        res.push_back({start, end}); // Add merged interval
+    }
+    return res;
+}
+
+
+// method2(optimal) t.c- O(nlogn + n)  s.c- O(n)
+vector<vector<int>> merge(vector<vector<int>> &intervals){
+    sort(intervals.begin(), intervals.end()); // Sort intervals based on starting time
+    vector<vector<int>> ans;               // Vector to store final merged intervals
+
+    for (auto interval : intervals){
+        if (ans.empty() || ans.back()[1] < interval[0]){ // If merged is empty or curr interval doesnt overlap
+            ans.push_back(interval); // Add curr interval as new non-overlapping block
+        }
+        else{
+            ans.back()[1] = max(ans.back()[1], interval[1]); // overlap exist: merge by extending the end time
+        }
+    }
+    return ans;
+}
