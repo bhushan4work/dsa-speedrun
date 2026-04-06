f6ee5a113d95e83a2125a03436d58a71caa27c74
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-04-06 19:06:50 +0530

    recursion-mid

---
+++ src/recursion/subseq_pattern/power_set.cpp
@@ -0,0 +1,30 @@
+// statement- find all possible subsequences of the str including str itself.
+
+
+// method1(brute) -using recursion t.c- O(n * 2^n)  s.c- O(n * 2^n)
+
+void helper(string &s, int index, string &current, vector<string> &result) {
+    if (index == s.size()) { // BASE CASE:If we have processed all char
+        result.push_back(current);  // store the curr subsequence
+        return;
+    }
+    
+    // STEP 1: EXCLUDE the curr char
+    helper(s, index + 1, current, result); // Move to next index without adding anything
+
+    // STEP 2: INCLUDE the current character
+    current.push_back(s[index]);   // choose the char
+
+    helper(s, index + 1, current, result);
+
+    current.pop_back(); // BACKTRACK: Remove the last added char to restore prev state
+}
+
+vector<string> getSubsequences(string s) {
+    vector<string> result;   // stores all subsequences
+    string current = "";     // temporary str
+
+    helper(s, 0, current, result);
+
+    return result;
+}
