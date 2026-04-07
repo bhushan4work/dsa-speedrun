63a95585bfb6b745ca4c5533207182251e6530a9
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-04-07 18:12:48 +0530

    added faq

--- src/recursion/subseq_pattern/subseq_with_sum_k.cpp
+++ src/recursion/subseq_pattern/subseq_with_sum_k.cpp
@@ -6,15 +6,36 @@ int func(int ind, int sum, vector<int> &nums) {
     if (ind == nums.size()) {  // reached end → check if valid
         return (sum == 0) ? 1 : 0;
     }
-
+    
     if (sum < 0) return 0;
-
+    
     int take = func(ind + 1, sum - nums[ind], nums); // take curr element
     int notTake = func(ind + 1, sum, nums);    // skip curr element
-
+    
     return take + notTake;
 }
 
 int countSubsequenceWithTargetSum(vector<int>& nums, int target) {
     return func(0, target, nums);
 }
+
+
+//statement- Return true if there exist subseq such that sum of all elements in subseq = k else false
+
+//method1(brute) t.c- O(2^n)  s.c- O(n)
+bool func(int ind, int sum, vector<int> &nums) {
+    if (sum == 0) return true; // if target achieved
+
+    if (ind == nums.size()) return false; // reached end → no subseq found
+
+    if (func(ind + 1, sum - nums[ind], nums)) return true; // take curr element
+
+    if (func(ind + 1, sum, nums)) return true; // skip curr element
+
+    return false;
+}
+
+bool subsetSumExists(vector<int>& nums, int target) {
+    return func(0, target, nums);
+}
+
