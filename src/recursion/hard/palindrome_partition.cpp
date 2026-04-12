f5068bc2fa5904916835ad794fa67e17486637f5
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-04-12 10:58:37 +0530

    recursion hard

---
+++ src/recursion/hard/palindrome_partition.cpp
@@ -0,0 +1,33 @@
+// statement- partition given str s such that every substr of partition is palindrome. Return all possible palindrome partitioning of s
+
+
+// method1(brute) t.c- O(2^n * n)  s.c- O(n)
+bool isPalindrome(string &s, int start, int end){
+    while (start <= end){
+        if (s[start++] != s[end--])
+            return false;
+    }
+    return true;
+}
+
+void solve(int index, string &s, vector<string> &path, vector<vector<string>> &result){
+    if (index == s.size()){ // base case
+        result.push_back(path);
+        return;
+    }
+
+    for (int i = index; i < s.size(); i++){
+        if (isPalindrome(s, index, i)){
+            path.push_back(s.substr(index, i - index + 1));
+            solve(i + 1, s, path, result);
+            path.pop_back(); // backtrack
+        }
+    }
+}
+
+vector<vector<string>> partition(string s){
+    vector<string> path;
+    vector<vector<string>> result;
+    solve(0, s, path, result);
+    return result;
+}
